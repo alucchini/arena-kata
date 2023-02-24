ead0759543ae5f1509b1eadb5bd0832048b41473
Antoine Lucchini <manokist@MacBook-Pro-de-Antoine.local>
2023-02-24 11:50:05 +0100

    add def and crtr pts

--- kotlin/src/test/kotlin/org/katas/ArenaDamageCalculatorTest.kt
+++ kotlin/src/test/kotlin/org/katas/ArenaDamageCalculatorTest.kt
@@ -7,22 +7,22 @@ class ArenaDamageCalculatorTest {
     @Test
     fun `should compute rest defender's lp for neutral fight`() {
         val attacker = Hero(HeroElement.Water, 100, 0, 0, 0, 100)
-        val defender = Hero(HeroElement.Water, 100, 0, 0, 0, 100)
-        assertEquals(0, getDefenderLp(attacker, defender))
+        val defender = Hero(HeroElement.Water, 100, 75, 0, 0, 100)
+        assertEquals(1, getDefenderLp(attacker, defender))
     }
 
     @Test
     fun `should compute rest defender's lp for a fight to the advantage of the defender`() {
         val attacker = Hero(HeroElement.Water, 100, 0, 0, 0, 100)
-        val defender = Hero(HeroElement.Earth, 100, 0, 0, 0, 100)
-        assertEquals(20, getDefenderLp(attacker, defender))
+        val defender = Hero(HeroElement.Earth, 100, 150, 0, 0, 100)
+        assertEquals(22, getDefenderLp(attacker, defender))
     }
 
     @Test
     fun `should compute rest defender's lp for a fight to the advantage of the attacker`() {
-        val attacker = Hero(HeroElement.Water, 50, 0, 0, 0, 100)
-        val defender = Hero(HeroElement.Fire, 100, 0, 0, 0, 100)
-        assertEquals(40, getDefenderLp(attacker, defender))
+        val attacker = Hero(HeroElement.Water, 50, 0, 0, 100, 100)
+        val defender = Hero(HeroElement.Fire, 100, 300, 0, 0, 100)
+        assertEquals(14, getDefenderLp(attacker, defender))
     }
 
     private fun getDefenderLp(attacker: Hero, defender: Hero): Int? {
