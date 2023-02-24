32954aeb9e44336ce3935c47690471059ba64c93
Antoine Lucchini <manokist@MacBook-Pro-de-Antoine.local>
2023-02-24 11:06:31 +0100

    calculation of the rest of the defender's life points after a fight with 2 heroes of the same type

--- kotlin/src/test/kotlin/org/katas/ArenaDamageCalculatorTest.kt
+++ kotlin/src/test/kotlin/org/katas/ArenaDamageCalculatorTest.kt
@@ -5,7 +5,32 @@ import org.junit.jupiter.api.Test
 
 class ArenaDamageCalculatorTest {
     @Test
-    fun `should fail`() {
-        assertEquals(4, 1 + 2)
+    fun `should compute rest defender's lp one water hero vs one water hero`() {
+        val attacker = Hero(HeroElement.Water, 100, 0, 0, 0, 100)
+        val defender = Hero(HeroElement.Water, 100, 0, 0, 0, 100)
+        val defenders = listOf(defender)
+        val arenaDamageCalculator = ArenaDamageCalculator()
+        val result = arenaDamageCalculator.computeDamage(attacker, defenders)
+        assertEquals(0, result?.get(0)?.lp)
+    }
+
+    @Test
+    fun `should compute rest defender's lp one fire hero vs one fire hero`() {
+        val attacker = Hero(HeroElement.Fire, 10, 0, 0, 0, 100)
+        val defender = Hero(HeroElement.Fire, 100, 0, 0, 0, 100)
+        val defenders = listOf(defender)
+        val arenaDamageCalculator = ArenaDamageCalculator()
+        val result = arenaDamageCalculator.computeDamage(attacker, defenders)
+        assertEquals(90, result?.get(0)?.lp)
+    }
+
+    @Test
+    fun `should compute rest defender's lp one earth hero vs one earth hero`() {
+        val attacker = Hero(HeroElement.Earth, 28, 0, 0, 0, 100)
+        val defender = Hero(HeroElement.Earth, 100, 0, 0, 0, 100)
+        val defenders = listOf(defender)
+        val arenaDamageCalculator = ArenaDamageCalculator()
+        val result = arenaDamageCalculator.computeDamage(attacker, defenders)
+        assertEquals(72, result?.get(0)?.lp)
     }
 }
