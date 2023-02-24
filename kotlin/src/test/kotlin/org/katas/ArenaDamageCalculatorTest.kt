192d7c49d849bfacd5ab2d3db143f8c216bc78c3
Antoine Lucchini <manokist@MacBook-Pro-de-Antoine.local>
2023-02-24 11:33:41 +0100

    refacto test

--- kotlin/src/test/kotlin/org/katas/ArenaDamageCalculatorTest.kt
+++ kotlin/src/test/kotlin/org/katas/ArenaDamageCalculatorTest.kt
@@ -8,29 +8,27 @@ class ArenaDamageCalculatorTest {
     fun `should compute rest defender's lp for neutral fight`() {
         val attacker = Hero(HeroElement.Water, 100, 0, 0, 0, 100)
         val defender = Hero(HeroElement.Water, 100, 0, 0, 0, 100)
-        val defenders = listOf(defender)
-        val arenaDamageCalculator = ArenaDamageCalculator()
-        val result = arenaDamageCalculator.computeDamage(attacker, defenders)
-        assertEquals(0, result?.get(0)?.lp)
+        assertEquals(0, getDefenderLp(attacker, defender))
     }
 
     @Test
     fun `should compute rest defender's lp for a fight to the advantage of the defender`() {
         val attacker = Hero(HeroElement.Water, 100, 0, 0, 0, 100)
         val defender = Hero(HeroElement.Earth, 100, 0, 0, 0, 100)
-        val defenders = listOf(defender)
-        val arenaDamageCalculator = ArenaDamageCalculator()
-        val result = arenaDamageCalculator.computeDamage(attacker, defenders)
-        assertEquals(20, result?.get(0)?.lp)
+        assertEquals(20, getDefenderLp(attacker, defender))
     }
 
     @Test
     fun `should compute rest defender's lp for a fight to the advantage of the attacker`() {
         val attacker = Hero(HeroElement.Water, 50, 0, 0, 0, 100)
         val defender = Hero(HeroElement.Fire, 100, 0, 0, 0, 100)
+        assertEquals(40, getDefenderLp(attacker, defender))
+    }
+
+    private fun getDefenderLp(attacker: Hero, defender: Hero): Int? {
         val defenders = listOf(defender)
         val arenaDamageCalculator = ArenaDamageCalculator()
         val result = arenaDamageCalculator.computeDamage(attacker, defenders)
-        assertEquals(40, result?.get(0)?.lp)
+        return result?.get(0)?.lp
     }
 }
