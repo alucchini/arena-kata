676da59c6f0c1bb59d8fb7712338e7599bcc79a8
Antoine Lucchini <manokist@MacBook-Pro-de-Antoine.local>
2023-02-26 19:06:44 +0100

    add 2 characteristics for Holy counter

--- kotlin/src/test/kotlin/org/katas/ArenaDamageCalculatorHolyTest.kt
+++ kotlin/src/test/kotlin/org/katas/ArenaDamageCalculatorHolyTest.kt
@@ -6,15 +6,34 @@ class ArenaDamageCalculatorHolyTest {
 
     @Test
     fun `hero not affected by advantage or disadvantage versus his adversary` () {
+        val defenderLP = 100
         val attacker = Hero(HeroElement.Fire, 100, 0, 0, 0, 100)
         attacker.apply {
             setCounters(mutableListOf(Counter.Holy))
         }
-        val defender = Hero(HeroElement.Fire, 20, 0, 0, 0, 100)
+        val defender = Hero(HeroElement.Fire, 20, 0, 0, 0, defenderLP)
         defender.apply {
             setBuffs(mutableListOf(Buff.Defense))
         }
         val result = ArenaDamageCalculator().computeDamage(attacker, listOf(defender))
-        assertEquals(25, result[0].lp)
+        if (attacker.getCounters().first() == Counter.Holy) {
+            defender.lp = (defenderLP - attacker.pow * 0.80).toInt()
+        }
+        assertEquals(20, result[0].lp)
+    }
+
+    @Test
+    fun `hero deals 20% less damage, but ignores the opponent's defense` () {
+        val defenderLP = 100
+        val attacker = Hero(HeroElement.Fire, 100, 0, 0, 0, 100)
+        attacker.apply {
+            setCounters(mutableListOf(Counter.Holy))
+        }
+        val defender = Hero(HeroElement.Fire, 20, 750, 0, 0, defenderLP)
+        val result = ArenaDamageCalculator().computeDamage(attacker, listOf(defender))
+        if (attacker.getCounters().first() == Counter.Holy) {
+            defender.lp = (defenderLP - attacker.pow * 0.80).toInt()
+        }
+        assertEquals(20, result[0].lp)
     }
 }
