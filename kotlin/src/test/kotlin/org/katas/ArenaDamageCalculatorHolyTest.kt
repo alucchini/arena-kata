4afb9195d0ee4e892f9be4d427763f41c2c39d44
Antoine Lucchini <manokist@MacBook-Pro-de-Antoine.local>
2023-02-27 22:17:52 +0100

    add holy counter to damage calculator + refacto holy test

--- kotlin/src/test/kotlin/org/katas/ArenaDamageCalculatorHolyTest.kt
+++ kotlin/src/test/kotlin/org/katas/ArenaDamageCalculatorHolyTest.kt
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test
 class ArenaDamageCalculatorHolyTest {
 
     @Test
-    fun `hero not affected by advantage or disadvantage versus his adversary` () {
+    fun `hero not affected by advantage or disadvantage versus his adversary, deals 20% less damage, but ignores the opponent's defense` () {
         val defenderLP = 100
         val attacker = Hero(HeroElement.Fire, 100, 0, 0, 0, 100)
         attacker.apply {
@@ -16,24 +16,21 @@ class ArenaDamageCalculatorHolyTest {
             setBuffs(mutableListOf(Buff.Defense))
         }
         val result = ArenaDamageCalculator().computeDamage(attacker, listOf(defender))
-        if (attacker.getCounters().first() == Counter.Holy) {
-            defender.lp = (defenderLP - attacker.pow * 0.80).toInt()
-        }
         assertEquals(20, result[0].lp)
     }
 
     @Test
-    fun `hero deals 20% less damage, but ignores the opponent's defense` () {
-        val defenderLP = 100
+    fun `hero choose what adversary he want` () {
         val attacker = Hero(HeroElement.Fire, 100, 0, 0, 0, 100)
         attacker.apply {
             setCounters(mutableListOf(Counter.Holy))
         }
-        val defender = Hero(HeroElement.Fire, 20, 750, 0, 0, defenderLP)
-        val result = ArenaDamageCalculator().computeDamage(attacker, listOf(defender))
-        if (attacker.getCounters().first() == Counter.Holy) {
-            defender.lp = (defenderLP - attacker.pow * 0.80).toInt()
-        }
-        assertEquals(20, result[0].lp)
+        val defenders = listOf<Hero>(
+            Hero(HeroElement.Fire, 20, 75, 0, 0, 200),
+            Hero(HeroElement.Water, 20, 7500, 0, 0, 40),
+            Hero(HeroElement.Earth, 20, 0, 0, 0, 500)
+        )
+        ArenaDamageCalculator().computeDamage(attacker, defenders, defenders[1])
+        assertEquals(0, defenders[1].lp)
     }
 }
