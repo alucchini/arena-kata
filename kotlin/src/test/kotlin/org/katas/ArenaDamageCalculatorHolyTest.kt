84d27d708253d227c018e070a724f6267c82be28
Antoine Lucchini <manokist@MBP-de-Antoine.corp.inflexsys.com>
2023-03-01 12:52:25 +0100

    refacto test holy

--- kotlin/src/test/kotlin/org/katas/ArenaDamageCalculatorHolyTest.kt
+++ kotlin/src/test/kotlin/org/katas/ArenaDamageCalculatorHolyTest.kt
@@ -7,12 +7,10 @@ class ArenaDamageCalculatorHolyTest {
     @Test
     fun `hero not affected by advantage or disadvantage versus his adversary, deals 20% less damage, but ignores the opponent's defense` () {
         val defenderLP = 100
-        val attacker = Hero(HeroElement.Fire, 100, 0, 0, 0, 100)
-        attacker.apply {
+        val attacker = Hero(HeroElement.Fire, 100, 0, 0, 0, 100).apply {
             setCounters(mutableListOf(Counter.Holy))
         }
-        val defender = Hero(HeroElement.Fire, 20, 0, 0, 0, defenderLP)
-        defender.apply {
+        val defender = Hero(HeroElement.Fire, 20, 0, 0, 0, defenderLP).apply {
             setBuffs(mutableListOf(Buff.Defense))
         }
         val result = ArenaDamageCalculator().computeDamage(attacker, listOf(defender))
@@ -21,8 +19,7 @@ class ArenaDamageCalculatorHolyTest {
 
     @Test
     fun `hero choose what adversary he want` () {
-        val attacker = Hero(HeroElement.Fire, 100, 0, 0, 0, 100)
-        attacker.apply {
+        val attacker = Hero(HeroElement.Fire, 100, 0, 0, 0, 100).apply {
             setCounters(mutableListOf(Counter.Holy))
         }
         val defenders = listOf<Hero>(
